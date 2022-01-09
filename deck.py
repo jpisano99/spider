a53f11be32b982c7a0f075229abaa3a301613d31
Jim <jpisano@cisco.com>
2022-01-09 13:35:44 -0500

    my first

--- deck.py
+++ deck.py
@@ -10,61 +10,93 @@ import hashlib
 
 
 class Card:
+    # Create a single card object of a specific value and suit
 
-    _id = 0
+    global_id = 0
 
-    def __init__(self, suit, value, card_type='random'):
-        suit_dict = {1: 'Spades', 2: 'Clubs', 3: 'Hearts', 4: 'Diamonds'}
+    def __init__(self, suit, value):
+        # Create a globally unique ID for this card
+        Card.global_id += 1
+
+        # Dicts for unicode card symbols and string names
+        glyph_dict = {'Spades': '\u2660', 'Clubs': '\u2663', 'Hearts': '\u2665', 'Diamonds': '\u2666'}
         value_dict = {1: 'Ace', 11: 'Jack', 12: 'Queen', 13: 'King'}
-        Card._id = Card._id + 1
 
-        d=hashlib.md5(b"hello worlds").digest()
-        d=base64.b64encode(d)
-        print(d)
-        exit()
+        # Create a string name for this card value
+        str_val = str(value)
+        if value == 1 or value >= 11:
+            str_val = value_dict[value]
 
+        # Create an MD5 Hash value
+        serial_bytes = bytes(suit, 'utf-8') + bytes(str(value), 'utf-8') + bytes(str(self.global_id), 'utf-8')
+        hash_val = hashlib.md5(serial_bytes).hexdigest()
 
-        if card_type == 'random':
-            self.id = Card._id,
-            self.card_type = card_type
-            self.suit = random.randrange(1, 5, 1)
-            self.value = random.randrange(1, 14, 1)
+        self.id = hash_val # MD5
+        self.global_id = self.global_id
+        self.suit_glyph = glyph_dict[suit]
+        self.suit = suit
+        self.str_val = str_val
+        self.int_value = value
+        self.visible = False
 
-            self.suit = suit_dict.get(self.suit)
-            self.value = value_dict.get(self.value, str(self.value))
-        else:
-            self.id = Card._id
-            self.suit = suit
-            self.value = value
-            self.card_type = card_type
+        # print('global id', Card.global_id, str_val + ' of ' + suit)
 
     def __repr__(self):
-        rep = 'Your Card is # ' + str(self.id) + ' is ' + self.value + ' of ' + self.suit
+        rep = 'Your Card is a ' + str(self.str_val) + \
+              ' of ' + self.suit + ' ' + self.suit_glyph + \
+              ' with a md5 hash of ' + str(self.id)
         return rep
 
 
 class Stack:
+    # Create a full suit cards (Ace to King) object of a specific suit
 
     def __init__(self, suit):
-        value_dict = {1: 'Ace', 11: 'Jack', 12: 'Queen', 13: 'King'}
-
-        card_values = [*range(1, 14)]
-        random.shuffle(card_values)
-
         self.cards = []
-        for idx in card_values:
-            self.cards.append([value_dict.get(idx, str(idx)), suit])
+
+        for idx in range(1, 14):
+            self.cards.append(Card(suit, idx))
+
+    def shuffle(self):
+        random.shuffle(self.cards)
 
     def __repr__(self):
-        rep = str(self.cards)
+        rep = 'This is a stack of ' + str(self.cards[1].suit)
+
         return rep
 
 
-class Deck(Card):
-    num_of_cards = 52
+class Deck:
+    # Create a full deck of standard cards
 
-    # def __init__(self, card_type):
-    #     print(card_type)
+    def __init__(self):
+        self.cards = []
+
+        suits = ['Diamonds', 'Spades', 'Hearts', 'Clubs']
+
+        for suit in suits:
+            for idx in range(1, 14):
+                self.cards.append(Card(suit, idx))
 
     def shuffle(self):
-        pass
+        random.shuffle(self.cards)
+
+    def __repr__(self):
+        rep = 'This is a deck of ' + str(len(self.cards)) + ' cards'
+        return rep
+
+
+
+
+
+# random.shuffle(card_values)
+
+# if card_type == 'random':
+#     self.id = hash_val
+#     self.card_type = card_type
+#     self.suit = random.randrange(1, 5, 1)
+#     self.value = random.randrange(1, 14, 1)
+#
+#     self.suit = suit_dict.get(self.suit)
+#     self.value = value_dict.get(self.value, str(self.value))
+#else:
